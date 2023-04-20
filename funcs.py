eb30edf5f879272ba3596cb5bb97ad5f6ec7d9b8
Yaroslav Galiulin <yaroslav66@list.ru>
2023-04-20 13:44:47 +0500

    add funcion minus

--- funcs.py
+++ funcs.py
@@ -3,7 +3,13 @@ def plus(x, y):
     return print("Сумма", z)
 
 
+def minus(x, y):
+    z = int(x) - int(y)
+    return print("Разность", z)
+
+
 a = input("1 число ")
 b = input("2 число ")
 
 plus(a, b)
+minus(a, b)
