947c214eac5db98ed02ba7a8565da4b082c83511
Yaroslav Galiulin <yaroslav66@list.ru>
2023-04-20 14:07:39 +0500

    add fun-ns "plus" "minus" from funcs

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from funcs import plus
+from funcs import minus
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+a = input("1 число ")
+b = input("2 число ")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plus(a, b)
+minus(a, b)
